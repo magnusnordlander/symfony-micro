72ebb29560522e845e9deb6d00715c2df8a63b7f
Magnus Nordlander <magnus@nordlander.se>
2012-06-09 16:30:24 +0200

    Initial commit of Symfony Micro

---
+++ README.md
@@ -0,0 +1,10 @@
+Symfony Micro Edition
+=====================
+
+Welcome to the unofficial Symfony Micro Edition.
+
+As opposed to the Standard edition, this is not fully functional, and if you've never used Symfony before, you should be using the Standard Edition instead.
+
+This is intended for experienced users of Symfony, and just includes the basics. That way, you don't have to rip out all the demo stuff and bundles you don't need for your project from the Standard Edition every time.
+
+If you don't know how to use this, don't. Use the Standard edition.
---
+++ app/AppKernel.php
@@ -0,0 +1,28 @@
+<?php
+
+use Symfony\Component\HttpKernel\Kernel;
+use Symfony\Component\Config\Loader\LoaderInterface;
+
+class AppKernel extends Kernel
+{
+    public function registerBundles()
+    {
+        $bundles = array(
+            new Symfony\Bundle\FrameworkBundle\FrameworkBundle(),
+            new Symfony\Bundle\SecurityBundle\SecurityBundle(),
+            new Symfony\Bundle\TwigBundle\TwigBundle(),
+            new Symfony\Bundle\MonologBundle\MonologBundle(),
+        );
+
+        if (in_array($this->getEnvironment(), array('dev', 'test'))) {
+            $bundles[] = new Symfony\Bundle\WebProfilerBundle\WebProfilerBundle();
+        }
+
+        return $bundles;
+    }
+
+    public function registerContainerConfiguration(LoaderInterface $loader)
+    {
+        $loader->load(__DIR__.'/config/config_'.$this->getEnvironment().'.yml');
+    }
+}
